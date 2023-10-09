ec17f07337ef8df48718ebb9788c5947f1040420
stanly08 <anasistanly@gmail.com>
2023-10-08 22:05:27 -0700

    pre-processor tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "2-main.c"
+#include "2-main.c"
+/** main - check the code
+ **
+ ** Return: Always 0.
+ **/
+
+int main(void)
+{
+	printf("Compiled from file: %s\n", __FILE__);
+	return (0);
+}
